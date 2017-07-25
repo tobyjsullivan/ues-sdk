6270f32298e3b870dba482e8717d02e5fa53c5ac
Toby Sullivan <tobyjsullivan@gmail.com>
2017-07-24 22:12:34 -0700

    Add support for event serialization

--- event/event.go
+++ event/event.go
@@ -1,6 +1,9 @@
 package event
 
-import "crypto/sha256"
+import (
+    "crypto/sha256"
+    "encoding/json"
+)
 
 type Event struct {
     PreviousEvent EventID
@@ -18,4 +21,46 @@ func (e *Event) ID() EventID {
     id := EventID(out)
 
     return id
+}
+
+func (e *Event) String() string {
+    prevId := e.PreviousEvent
+    data := e.Data
+
+    ser := eventFormat{
+        PreviousID: prevId.String(),
+        Type: e.Type,
+        Data: data.String(),
+    }
+    b, err := json.Marshal(&ser)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    return string(b)
+}
+
+func (e *Event) Parse(s string) error {
+    var ser eventFormat
+    err := json.Unmarshal([]byte(s), &ser)
+    if err != nil {
+        return err
+    }
+
+    data, err := ParseData(ser.Data)
+    if err != nil {
+        return err
+    }
+
+    e.PreviousEvent.Parse(ser.PreviousID)
+    e.Type = ser.Type
+    e.Data = data
+
+    return nil
+}
+
+type eventFormat struct {
+    PreviousID string `json:"previousId"`
+    Type string `json:"type"`
+    Data string `json:"data"`
 }
